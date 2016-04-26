a84efe813a22cb1439423cf7284f2e126fef30a7
Heipiao <lsl8315@gmail.com>
2016-04-26 15:11:05 +0000

    dd

--- test1.py
+++ test1.py
@@ -2,16 +2,15 @@ from weibo import APIClient
 import json
 
 APP_KEY = "3722673574"
-APP_SECTER = "3686fea0a65da883b6c2a7586f350425"
+APP_SECRET = "3686fea0a65da883b6c2a7586f350425"
 CALLBACK_URL = 'https://api.weibo.com/oauth2/default.html'     
-#
 client = APIClient(app_key=APP_KEY, app_secret=APP_SECRET, redirect_uri=CALLBACK_URL)
 
 with open('token.json', 'r') as f:
     r = json.load(f)
-access_token = r.access_token
-expires_in = r.expires_in 
-#设置得到的access_token
+access_token = r["access_token"]
+expires_in = r["expires_at"]
 client.set_access_token(access_token, expires_in)
-a = client.statuses__public_timeline(200)
-print a
+raw_data  = client.statuses.public_timeline.get(count=200)
+for st in raw_data.statuses:
+    print st.encode("utf-8").text
